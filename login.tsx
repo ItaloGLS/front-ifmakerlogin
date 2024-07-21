c9de631cbdc6caa5c6b20205a08ddd87c9544916
Italo Gabriel <95325429+ItaloGLS@users.noreply.github.com>
2024-07-21 15:07:09 -0300

    Create login.tsx

---
+++ login.tsx
@@ -0,0 +1,47 @@
+// src/paginas/Login.tsx
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { login } from '../auth';
+
+const Login: React.FC = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      await login(email, password);
+      navigate('/paineldecontrole');
+    } catch (error) {
+      setError('Login failed');
+    }
+  };
+
+  return (
+    <div className="login">
+      <div className='cardlogin'>
+        <h2>Login - Painel de Controle</h2>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit">Login</button>
+          {error && <p>{error}</p>}
+      </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
